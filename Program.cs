c60aee1018f1c1fec64d11dae2d4179127310b0e
fthann <fathanalfrq@gmail.com>
2024-10-21 11:25:50 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // For Loop
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
